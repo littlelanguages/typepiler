6f3d64ca5504120062113439d589abf372600025
Graeme Lockley <graemel@no9.co.za>
2020-11-13 20:31:43 +0000

    test: parse set declaration

--- parser/parser_test.ts
+++ parser/parser_test.ts
@@ -1,5 +1,11 @@
 import { assertEquals } from "../testing/asserts.ts";
-import { Declarations, Reference, UnionDeclaration } from "./ast.ts";
+import {
+  Declarations,
+  Name,
+  Reference,
+  SetDeclaration,
+  UnionDeclaration,
+} from "./ast.ts";
 import { parse } from "./parser.ts";
 
 Deno.test("parser - alias union", () => {
@@ -17,5 +23,23 @@ Deno.test("parser - alias union", () => {
   ["B", "C", "D"].forEach((n, i) => {
     assertEquals(unionDeclaration.elements[i].tag, "Reference");
     assertEquals((unionDeclaration.elements[i] as Reference).name.id, n);
+  });
+});
+
+Deno.test("parser - set declaration", () => {
+  const result: Declarations = parse("A = {B, C, D};").either(
+    (_) => [],
+    (r) => r,
+  );
+
+  assertEquals(result.length, 1);
+  assertEquals(result[0].name.id, "A");
+  assertEquals(result[0].tag, "SetDeclaration");
+
+  const setDeclaration = result[0] as SetDeclaration;
+  assertEquals(setDeclaration.elements.length, 3);
+  ["B", "C", "D"].forEach((n, i) => {
+    assertEquals(setDeclaration.elements[i].tag, "Name");
+    assertEquals((setDeclaration.elements[i] as Name).id, n);
   });
 });
